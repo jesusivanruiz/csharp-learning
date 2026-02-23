7dbdb7354d99a26c867a6cd58458a33f441f50c5
JesusIvanRuiz <jevanruiz@gmail.com>
2026-02-22 19:31:46 -0600

    Fix: convertir submódulos a carpetas normales

---
+++ Bucles/Program.cs
@@ -0,0 +1,13 @@
+﻿int attempts = 1;
+while (attempts <= 3)
+{    Console.Write($"Attempt: {attempts} \n");
+    attempts++; 
+}
+    Console.WriteLine("End of loop.");
+
+List<string> languages = new List<string> { "C#", "Python", "JavaScript" };
+
+  foreach (string language in languages)
+  {
+      Console.WriteLine($"Language: {language}");
+  }
---
+++ Whats-your-name/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine() ?? "Usuario";
+Console.WriteLine("Ingrese su edad: ");
+int edad = Convert.ToInt32(Console.ReadLine());
+if(edad<=13)
+{
+    Console.WriteLine("Eres un niño.");
+}
+else if(edad>13 && edad<=17)
+{
+    Console.WriteLine("Eres un adolescente.");
+}
+else if(edad>17 && edad<=64)
+{
+    Console.WriteLine("Eres un adulto.");
+}
+else
+{
+    Console.WriteLine("Eres un adulto mayor.");
+}
+
