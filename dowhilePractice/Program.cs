b9977e2bf2043892d81c43f1be020029df7829aa
JesusIvanRuiz <jevanruiz@gmail.com>
2026-03-04 08:20:07 -0600

    Add doWhile Lesstan 1 and over 10 condition as a invalid input

--- dowhilePractice/Program.cs
+++ dowhilePractice/Program.cs
@@ -7,11 +7,17 @@ namespace TestApp
         static void Main(string[] args)
         {
             bool numberProgram = true;
-            do {
+            do
+            {
                 Console.WriteLine("Enter a number:");
                 if (!int.TryParse(Console.ReadLine(), out int number))
                 {
                     Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+                if (number < 1 || number > 10)
+                {
+                    Console.WriteLine("Fuera de rango, intenta de nuevo");
                     continue;
                 }
                 Console.WriteLine($"You entered: {number}");
