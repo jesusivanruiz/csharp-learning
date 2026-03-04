53915bb4123b221b05995f7e1e37eaee569ea057
JesusIvanRuiz <jevanruiz@gmail.com>
2026-03-04 07:57:06 -0600

    Add DoWhile Practice

---
+++ dowhilePractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool numberProgram = true;
+            do {
+                Console.WriteLine("Enter a number:");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+                Console.WriteLine($"You entered: {number}");
+                numberProgram = false;
+
+            } while (numberProgram);
+        }
+    }
+}
